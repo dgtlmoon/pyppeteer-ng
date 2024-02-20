641ba825a6a0778ec100e4978f05d40d64619126
dgtlmoon <dgtlmoon@gmail.com>
2024-02-20 19:51:17 +0100

    2.0.0rc4

--- pyppeteer/__init__.py
+++ pyppeteer/__init__.py
@@ -9,7 +9,7 @@ from appdirs import AppDirs
 
 __author__ = 'Hiroyuki Takagi, Bernardas Ališauskas, Matt Marcus'
 __email__ = 'pyppeteer@protonmail.com'
-__version__ = '2.0.0rc2'
+__version__ = '2.0.0rc4'
 __chromium_revision__ = '722234'
 __base_puppeteer_version__ = 'v2.1.1'
 __pyppeteer_home__ = os.environ.get('PYPPETEER_HOME', AppDirs('pyppeteer').user_data_dir)  # type: str
